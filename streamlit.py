3b8313da39bbf2485319fd27a736cf9eb37e7798
yaswanthpusuluri <155798848+yaswanthpusuluri@users.noreply.github.com>
2024-01-06 10:58:23 +0530

    Add files via upload

---
+++ streamlit.py
@@ -0,0 +1,33 @@
+
+
+import streamlit as st
+import pickle
+
+model=pickle.load(open('model.pkl','rb'))
+st.title('Fraud Detection ')
+
+AM=st.text_input('Enter the Amount: ')
+HD=st.text_input('Enter the HoursofDay: ')
+DW=st.text_input('Enter the DayOfWeak: ')
+MC=st.text_input('Enter the MerchantCategory : ')
+IN=st.text_input('Enter it is international fraud or not : ')
+
+
+
+if st.button('Predict'):
+    AM=float(AM)
+    HD=float(HD)
+    DW=float(DW)
+    MC=int(MC)
+    IN=int(IN)
+   
+    data=[[AM,HD,DW,MC,IN]]
+    result=model.predict(data)
+    st.success(result)
+
+    if result[0]==0:
+        st.write("Fraud is not detected")
+
+
+    else :
+        st.write("fraud is detected")
